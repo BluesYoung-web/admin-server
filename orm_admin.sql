04cde06832fb2cc204ecaaad6f8d97841780292a
张扬 <15171255945@163.com>
2021-07-14 11:41:32 +0800

    chore: :hammer: 数据库变更

--- orm_admin.sql
+++ orm_admin.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50726
 File Encoding         : 65001
 
-Date: 2021-07-13 16:09:04
+Date: 2021-07-14 11:39:34
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -31,21 +31,21 @@ CREATE TABLE `node` (
   PRIMARY KEY (`autoid`),
   KEY `FK_3341dbe0417c4e6f7a41de5702a` (`parentIdAutoid`),
   CONSTRAINT `FK_3341dbe0417c4e6f7a41de5702a` FOREIGN KEY (`parentIdAutoid`) REFERENCES `node` (`autoid`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Records of node
 -- ----------------------------
 INSERT INTO `node` VALUES ('0', '0', '根节点', '', '', '0', '0', null);
-INSERT INTO `node` VALUES ('1', '1', '系统管理', '系统管理', '', '0', '1', '0');
-INSERT INTO `node` VALUES ('2', '1', '自定义组件', '常用组件', '', '-1', '1', '0');
-INSERT INTO `node` VALUES ('3', '1', '', 'VueUse使用示例', '', '-2', '1', '0');
-INSERT INTO `node` VALUES ('5', '1', '', '节点列表', '10000/5', '0', '2', '1');
-INSERT INTO `node` VALUES ('6', '1', '', '角色列表', '10000/8', '-1', '2', '1');
-INSERT INTO `node` VALUES ('7', '1', '', '管理员列表', '10000/12', '-2', '2', '1');
+INSERT INTO `node` VALUES ('1', '1', '系统管理', '系统管理', '', '-1', '1', '0');
+INSERT INTO `node` VALUES ('2', '1', '自定义组件', '常用组件', '', '-2', '1', '0');
+INSERT INTO `node` VALUES ('3', '1', '', 'VueUse使用示例', '', '-3', '1', '0');
+INSERT INTO `node` VALUES ('5', '1', '', '节点列表', '10000/5', '-3', '2', '1');
+INSERT INTO `node` VALUES ('6', '1', '', '角色列表', '10000/8', '-4', '2', '1');
+INSERT INTO `node` VALUES ('7', '1', '', '管理员列表', '10000/12', '-5', '2', '1');
 INSERT INTO `node` VALUES ('8', '0', '新增 | 编辑', '新增 | 编辑', '10000/6', '0', '3', '5');
 INSERT INTO `node` VALUES ('9', '0', '删除', '删除', '10000/7', '0', '3', '5');
-INSERT INTO `node` VALUES ('12', '1', '自动创建测试', '自动创建测试', '', '-3', '1', '0');
+INSERT INTO `node` VALUES ('12', '1', '自动创建测试', '自动创建测试', '', '-4', '1', '0');
 INSERT INTO `node` VALUES ('13', '0', '添加 | 编辑', '添加 | 编辑', '10000/9', '0', '3', '6');
 INSERT INTO `node` VALUES ('14', '0', '删除', '删除', '10000/10', '0', '3', '6');
 INSERT INTO `node` VALUES ('15', '0', '获取节点权限列表', '获取节点权限列表', '10000/11', '0', '3', '6');
@@ -59,6 +59,10 @@ INSERT INTO `node` VALUES ('22', '1', '富文本编辑器', '富文本编辑器'
 INSERT INTO `node` VALUES ('23', '1', 'useEventListener', 'useEventListener', '10000/20', '0', '2', '3');
 INSERT INTO `node` VALUES ('24', '1', '页面创建测试', '页面创建测试', '10000/21', '0', '2', '12');
 INSERT INTO `node` VALUES ('25', '0', '文件上传接口', '文件上传接口', '10000/22', '0', '3', '21');
+INSERT INTO `node` VALUES ('26', '0', '', '获取权限列表', '10000/3', '0', '2', '29');
+INSERT INTO `node` VALUES ('27', '0', '退出登录', '退出登录', '10000/2', '-1', '2', '29');
+INSERT INTO `node` VALUES ('28', '0', '修改密码', '修改密码', '10000/4', '-2', '2', '29');
+INSERT INTO `node` VALUES ('29', '0', 'base', 'base', '', '0', '1', '0');
 
 -- ----------------------------
 -- Table structure for role
@@ -79,8 +83,8 @@ CREATE TABLE `role` (
 -- Records of role
 -- ----------------------------
 INSERT INTO `role` VALUES ('1', '1', '0', '0', '超级管理员', '超级管理员', '*');
-INSERT INTO `role` VALUES ('2', '1', '0', '0', '产品', '产品', '1,5,2,19,20,21,22,3,23,12,24');
-INSERT INTO `role` VALUES ('3', '1', '0', '0', '游客', '游客', '2,19,20,21,22,3,23,12,24');
+INSERT INTO `role` VALUES ('2', '1', '0', '0', '产品', '产品', '29,26,27,28,1,5,2,19,20,21,22,3,23,12,24');
+INSERT INTO `role` VALUES ('3', '1', '0', '0', '游客', '游客', '29,26,27,28,2,19,20,21,22,3,23,12,24');
 
 -- ----------------------------
 -- Table structure for user
